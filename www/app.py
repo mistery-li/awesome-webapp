6f918554587e7f08722f1b6c352fe55dffba9a46
mistery-li <853402477@qq.com>
2017-06-25 14:05:11 +0800

    new webapp demo

--- www/app.py
+++ www/app.py
@@ -13,7 +13,7 @@ def init(loop):
     app = web.Application(loop=loop)
     app.router.add_route('GET', '/', index)
     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 5000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    logging.info('server started at http://127.0.0.1:5000...')
     return srv
 
 loop = asyncio.get_event_loop()
